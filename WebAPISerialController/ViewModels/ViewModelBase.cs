211dc4b78b189b0394311a49071a3a9144956156
Winfried Rothenberg <155566996+winne-proaudio@users.noreply.github.com>
2025-04-19 20:58:25 +0200

    Add files via upload

---
+++ WebAPISerialController/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace WebAPISerialController.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
---
+++ WebAPISerialController/Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+
+namespace WebAPISerialController.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
